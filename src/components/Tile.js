99a3db1784233dea14c7a03f025722f2dc10910b
Davide Ensini <davide.DE.ensini@partner.bmw.de>
2020-11-16 16:25:23 +0100

    Make selected tiles shake

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Tile extends React.Component {
     render() {   
         return (
-            <div className="Tile" style={{width: 50 + 'px', height: 50 + 'px', backgroundColor: this.props.color, border:this.props.selected ? '3px solid black' : '0px'}}>
+            <div className={`Tile ${this.props.selected ? 'shaking' : ''}`}  style={{backgroundColor: this.props.color }}>
             </div>
         );
     }
